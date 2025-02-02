4ef8746fdc0cb16ada3877a9a54003e8236b8ba9
Mustafa Olmezses <mustafaolmezses@gmail.com>
2025-02-02 21:54:51 +0300

    Initial Commit

---
+++ VersionControlSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VersionControlSwiftUI
+//
+//  Created by Mustafa Ölmezses on 2.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VersionControlSwiftUI/VersionControlSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VersionControlSwiftUIApp.swift
+//  VersionControlSwiftUI
+//
+//  Created by Mustafa Ölmezses on 2.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct VersionControlSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
